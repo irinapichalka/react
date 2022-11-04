6af63bf459d1f7e1d24deffbf20855a4db872b04
irinapichalka <irinarescan@gmail.com>
2022-11-04 10:21:49 +0200

    l1-t2

---
+++ lesson1/task2/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+  'div',
+  { className: 'greeting' },
+  React.createElement('div', { className: 'greeting__title' }, 'Hello, world!'),
+  React.createElement(
+    'div',
+    { className: 'greeting__text' },
+    "I'm, learning React"
+  )
+);
+
+ReactDOM.render(greetingElem, rootElement);
