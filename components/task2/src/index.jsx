96582b44346dc45bad6d48f1bdc6b778d7f78ab0
irinapichalka <irinarescan@gmail.com>
2022-11-09 19:46:13 +0200

    t2

--- components/task2/src/index.jsx
+++ components/task2/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import Search from './Search';
+import Search from './Search.jsx';
 
 const rootElement = document.querySelector('#root');
 
