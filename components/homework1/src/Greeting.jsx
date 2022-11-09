47eca7da493f9cd3c4c21f154fdff04a56bfd286
irinapichalka <irinarescan@gmail.com>
2022-11-09 20:52:58 +0200

    home1

--- components/homework1/src/Greeting.jsx
+++ components/homework1/src/Greeting.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+const getAge = (date) => {
+  const today = new Date();
+  const birthDate = new Date(date);
+  let age;
+  age = today.getFullYear() - birthDate.getFullYear();
+  let m = today.getMonth() - birthDate.getMonth();
+  if(m<0 || (m===0 && today.getDate() < birthDate.getDate() )){
+    age--;
+  }
+  return age;
+};
 function Greeting(props) {
+  const age = getAge(props.user.birthDate);
   return (
     <div className='greeting'>
-      {`My name is ${props.name}. I'm ${props.age} years old`}
+      {`My name is ${props.user.firstName} ${props.user.lastName}. I'm ${age} years old`}
     </div>
   );
 }
--- components/homework1/src/index.jsx
+++ components/homework1/src/index.jsx
@@ -5,4 +5,10 @@ import Greeting from './Greeting.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Greeting name='John Doe' age='17' />, rootElement);
+const user = {
+  firstName: 'John',
+  lastName: 'Doe',
+  birthDate: new Date('December 17, 2021 03:24:00'),
+};
+
+ReactDOM.render(<Greeting user={user} />, rootElement);
