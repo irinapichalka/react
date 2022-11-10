9760524f3f9d999fef98d0a26bf3cc91ba455cad
irinapichalka <irinarescan@gmail.com>
2022-11-10 18:56:07 +0200

    h2

---
+++ events/homework2/src/ColorPicker.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class ColorPicker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+  showName = (color) => {
+    this.setState({
+      text: color,
+    });
+  };
+  hideName = (color) => {
+    this.setState({
+      text: '',
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div class='picker__title'>{this.state.text}</div>
+        <div>
+          <button
+            className='picker__button picker__button_coral'
+            onMouseEnter={() => this.showName('Coral')}
+            onMouseLeave={() => this.hideName()}
+          ></button>
+          <button
+            className='picker__button picker__button_aqua'
+            onMouseEnter={() => this.showName('Aqua')}
+            onMouseLeave={() => this.hideName()}
+          ></button>
+          <button
+            className='picker__button picker__button_bisque'
+            onMouseEnter={() => this.showName('Bisque')}
+            onMouseLeave={() => this.hideName()}
+          ></button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
