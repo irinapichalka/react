f9d4aee37f3f7a15ed83ca429eb0bb500ca2bd7f
irinapichalka <irinarescan@gmail.com>
2022-11-10 11:39:04 +0200

    counter

---
+++ state/task1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Counter from './Counter.jsx';
+
+const App = () => {
+  return (
+    <>
+      <Counter start={17} interval={1000} />
+      <Counter start={-50} interval={100} />
+      <Counter start={0} interval={1000} />
+    </>
+  );
+};
+
+export default App;
