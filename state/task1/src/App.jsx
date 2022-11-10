e467cefcbb8651d3bd787b49dc957885457c721c
irinapichalka <irinarescan@gmail.com>
2022-11-10 13:45:04 +0200

    clock

--- state/task1/src/App.jsx
+++ state/task1/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Counter from './Counter.jsx';
+import Clock from './Clock.jsx';
 
 const App = () => {
   return (
     <>
-      <Counter start={17} interval={1000} />
-      <Counter start={-50} interval={100} />
-      <Counter start={0} interval={1000} />
+      <Clock location='London' offset={0} />
+      <Clock location='Kyiv' offset={2} />
+      <Clock location='New York' offset={-5} />
     </>
   );
 };
---
+++ state/task1/src/Clock.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './clock.scss';
+
+const getTimeWithOffset = (offset) => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(
+    currentTime.setHours(currentTime.getHours() + offset + utcOffset)
+  );
+};
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: getTimeWithOffset(props.offset),
+      location: props.location,
+    };
+    // Do not do this way
+    setInterval(() => {
+      this.setState({
+        time: getTimeWithOffset(props.offset),
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className='clock'>
+        <div className='clock__location'>{this.location}</div>
+        <div className='clock__time'>
+          {new Date(this.state.time).toString()}
+        </div>
+      </div>
+    );
+  }
+}
+export default Clock;
