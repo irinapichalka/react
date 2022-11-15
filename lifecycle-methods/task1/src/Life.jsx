0c0045244d93791b87995af2885144bb049be1de
irinapichalka <irinarescan@gmail.com>
2022-11-15 10:45:55 +0200

    t1

--- lifecycle-methods/task1/src/Life.jsx
+++ lifecycle-methods/task1/src/Life.jsx
@@ -14,6 +14,7 @@ class Life extends React.Component {
     console.log(
       'shouldComponentUpdate(nextProps, nextState): decide to render or not to render'
     );
+    return true;
   }
 
   componentDidUpdate(prevProps, prevState) {
