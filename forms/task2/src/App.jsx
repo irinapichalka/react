d4d83e8cfff4a1c3ebe49a37bc3c7beda3c80aab
irinapichalka <irinarescan@gmail.com>
2022-11-16 14:40:39 +0200

    t2

---
+++ forms/task2/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import UserForm from './UserForm';
+
+const App = () => {
+  const createUser = (userData) => {
+    console.log(userData);
+  };
+  return <UserForm onSubmit={createUser} />;
+};
+
+export default App;
