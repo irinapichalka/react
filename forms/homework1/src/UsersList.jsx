1a962162fea0374a8ec88289516565f343a6827b
irinapichalka <irinarescan@gmail.com>
2022-11-17 12:20:40 +0200

    h1

--- forms/homework1/src/UsersList.jsx
+++ forms/homework1/src/UsersList.jsx
@@ -7,7 +7,6 @@ class UsersList extends React.Component {
     super(props);
 
     this.state = {
-      users: this.props.users,
       text: '',
     };
   }
@@ -15,27 +14,7 @@ class UsersList extends React.Component {
   handleChange = (event) => {
     const { value } = event.target;
 
-    if (value === '') {
-      this.setState({
-        users: this.props.users,
-        text: '',
-      });
-    }
-    const newUsers = this.props.users.filter((user) => {
-      return (
-        user.name.toUpperCase() === value.toUpperCase() || user.age === value
-      );
-    });
-
-    if (newUsers.length !== 0) {
-      this.setState({
-        users: newUsers,
-      });
-    } else {
-      this.setState({
-        users: [],
-      });
-    }
+    //if(value === '')
 
     this.setState({
       text: value,
@@ -43,16 +22,29 @@ class UsersList extends React.Component {
   };
 
   render() {
-    const htmlForUsers = this.state.users.map((user) => (
-      <User name={user.name} age={user.age} key={user.id} />
-    ));
+    let htmlForUsers = [];
+
+    if (this.state.text === '') {
+      htmlForUsers = this.props.users.map((user) => (
+        <User name={user.name} age={user.age} key={user.id} />
+      ));
+    } else {
+      htmlForUsers = this.props.users
+        .filter((user) => {
+          return (
+            user.name.toUpperCase() === this.state.text.toUpperCase() ||
+            user.age === this.state.text
+          );
+        })
+        .map((user) => <User name={user.name} age={user.age} key={user.id} />);
+    }
 
     return (
       <div>
         <Filter
           onChange={this.handleChange}
           filterText={this.state.text}
-          count={this.state.users.length}
+          count={htmlForUsers.length}
         />
         <ul className='users'>{htmlForUsers}</ul>
       </div>
