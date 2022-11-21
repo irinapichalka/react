dab46d3e2e7dc0ab1906b537b9afa8d715c4dec8
irinapichalka <irinarescan@gmail.com>
2022-11-21 20:51:54 +0200

    h

--- expanded-component/homework/src/App.jsx
+++ expanded-component/homework/src/App.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
 import Expand from './Expand';
 
-class App extends React.Component {
-  state = {
-    toShow: false,
-  };
-
-  onChangeState = () => {
-    console.log(this.state.toShow);
-    if (this.state.toShow) {
-      this.setState({
-        toShow: false,
-      });
-    } else {
-      this.setState({
-        toShow: true,
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div className='app'>
-        <Expand
-          title='Some text'
-          onChangeState={this.onChangeState}
-          toShow={this.state.toShow}
-        >
-          <p>
-            Hooks are a new addition in React 16.8. They let you use state and
-            other React features without writing a class.
-          </p>
-        </Expand>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className='app'>
+      <Expand title='Some text'>
+        <p>
+          Hooks are a new addition in React 16.8. They let you use state and
+          other React features without writing a class.
+        </p>
+      </Expand>
+    </div>
+  );
+};
 
 export default App;
--- expanded-component/homework/src/Expand.jsx
+++ expanded-component/homework/src/Expand.jsx
@@ -1,25 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Expand = ({ children, title, onChangeState, toShow }) => {
-  const classForIcon = toShow ? 'fa-chevron-up' : 'fa-chevron-down';
-  return (
-    <div className='expand border'>
-      <div className='expand__header'>
-        <span className='expand__title'>{title}</span>
-        <button className='expand__toggle-btn' onClick={onChangeState}>
-          <i className={`fas ${classForIcon}`}></i>
-        </button>
+class Expand extends React.Component {
+  state = {
+    toShow: false,
+  };
+
+  onChangeState = () => {
+    if (this.state.toShow) {
+      this.setState({
+        toShow: false,
+      });
+    } else {
+      this.setState({
+        toShow: true,
+      });
+    }
+  };
+  render() {
+    const classForIcon = this.state.toShow
+      ? 'fa-chevron-up'
+      : 'fa-chevron-down';
+    const { title, children } = this.props;
+
+    return (
+      <div className='expand border'>
+        <div className='expand__header'>
+          <span className='expand__title'>{title}</span>
+          <button className='expand__toggle-btn' onClick={this.onChangeState}>
+            <i className={`fas ${classForIcon}`}></i>
+          </button>
+        </div>
+        <div
+          className='expand__content'
+          style={{ display: this.state.toShow ? 'block' : 'none' }}
+        >
+          {children}
+        </div>
       </div>
-      <div
-        className='expand__content'
-        style={{ display: toShow ? 'block' : 'none' }}
-      >
-        {children}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Expand.propTypes = {
   children: PropTypes.element.isRequired,
