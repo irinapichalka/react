824abb06565b917f8e29536590e18f0ce75103f3
irinapichalka <irinarescan@gmail.com>
2022-11-22 15:38:11 +0200

    todo

--- homework/todo-list/src/index.js
+++ homework/todo-list/src/index.js
@@ -1,22 +1,7 @@
 // should render following html
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import './list.css';
+import './index.scss';
+import App from './App';
 
-const rootElement = document.querySelector('#root');
-
-const element = (
-  <>
-    <h1 className='title'>Todo List</h1>
-    <main className='todo-list'>
-      <div className='actions'>
-        <input className='task-input' type='text' />
-        <button className='btn create-task-btn'>Create</button>
-      </div>
-      <ul className='list'></ul>
-    </main>
-  </>
-);
-
-ReactDOM.render(element, rootElement);
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ homework/todo-list/src/tasksGateway.js
@@ -0,0 +1,53 @@
+const baseUrl = 'https://635674979243cf412f8501b0.mockapi.io/api/v1/tasks';
+
+export const createTask = (taskData) => {
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;utc-8',
+    },
+    body: JSON.stringify(taskData),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+};
+
+export const fetchTasksList = () => {
+  return fetch(baseUrl).then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  });
+  //.then((tasksList) =>
+  // tasksList.map(({ _id, ...task }) => ({
+  //  id: _id,
+  //  ...task,
+  //}))
+  //);
+};
+
+export const deleteTask = (taskId) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'DELETE',
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to delete task');
+    }
+  });
+};
+
+export const updateTask = (taskId, taskData) => {
+  return fetch(`${baseUrl}/${taskId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json;utc-8',
+    },
+    body: JSON.stringify(taskData),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error('Failed to create task');
+    }
+  });
+};
