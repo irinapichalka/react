3e2eda22251d10b0a3bab815ec9674999f00bc6b
irinapichalka <irinarescan@gmail.com>
2022-11-23 17:58:43 +0200

    t2

--- router/task2/src/Product.jsx
+++ router/task2/src/Product.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 function Product({ match }) {
-  return (
-    <div className='product'>{`Product is a ${match.params.productId}`}</div>
-  );
+  return <div className='product'>{match.params.productId}</div>;
 }
 
 export default Product;
