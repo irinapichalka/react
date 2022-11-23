62b43eeea6c4e2d36912a64a3cbe6aa6916033d6
irinapichalka <irinarescan@gmail.com>
2022-11-23 17:36:00 +0200

    t2

--- router/task2/src/Product.jsx
+++ router/task2/src/Product.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
 
-function Product() {
-  const { productId } = useParams();
-  return <div className='product'>{`Product is a ${productId}`}</div>;
+function Product({ match }) {
+  return (
+    <div className='product'>{`Product is a ${match.params.productId}`}</div>
+  );
 }
 
 export default Product;
--- router/task2/src/Products.jsx
+++ router/task2/src/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Switch, useParams } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import Product from './Product';
 
 function Products({ match }) {
@@ -18,9 +18,7 @@ function Products({ match }) {
         <Route exact path={match.url}>
           <span>Select a product please</span>
         </Route>
-        <Route path={`${match.url}/:productId`}>
-          <Product />
-        </Route>
+        <Route path={`${match.url}/:productId`} component={Product} />
       </Switch>
     </div>
   );
