a406b33552d2380029527775477cbd288043c014
irinapichalka <irinarescan@gmail.com>
2022-11-23 13:04:57 +0200

    t1

---
+++ router/task1/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Contacts from './Contacts';
+
+const App = () => {
+  return (
+    <div className='page'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/products'>
+            <Products />
+          </Route>
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
