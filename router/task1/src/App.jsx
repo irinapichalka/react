96b97b91dac64a46af496390d6d6deb2e04c322d
irinapichalka <irinarescan@gmail.com>
2022-11-23 13:09:24 +0200

    t1

--- router/task1/src/App.jsx
+++ router/task1/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Products from './Products';
 import Contacts from './Contacts';
+import PageNotFound from './PageNotFound';
 
 const App = () => {
   return (
@@ -17,6 +18,9 @@ const App = () => {
           </Route>
           <Route path='/contacts'>
             <Contacts />
+          </Route>
+          <Route path='*'>
+            <PageNotFound />
           </Route>
         </Switch>
       </BrowserRouter>
